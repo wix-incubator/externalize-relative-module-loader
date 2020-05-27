0657d574216b69826661e8e66dbc28736543cdfd
Artem Yavorsky <aqson@me.com>
2020-05-27 20:03:00 +0300

    Fix dotted override

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-module.exports = function (content) {
-  const externalModule = extractModule(this.context);
-  const actualModule = extractModule(this.resourcePath).replace(/.js$/, '');
+const lodash = require('lodash');
+
+// camelcase: camelCase
+const lowerCaseToCamelCaseMap = Object.keys(lodash).reduce((map, key) => {
+  map[key.toLowerCase()] = key;
+  return map;
+}, {});
+
+module.exports = function(content) {
+  let externalModule = extractModule(this.context);
+  let actualModule;
+
+  if (isDotted(externalModule)) {
+    // 'lodash.camelcase'
+    [externalModule, actualModule] = externalModule.split(/\./);
+    actualModule = lowerCaseToCamelCaseMap[actualModule] || actualModule;
+  } else {
+    // 'lodash/camelCase'
+    actualModule = extractModule(this.resourcePath).replace(/.js$/, '');
+  }
 
   return `module.exports = require('${externalModule}').${actualModule}`;
 };
 
 function extractModule(path) {
   return path.split(/[\\/]/).pop();
+}
+
+function isDotted(externalModule) {
+  return /\w+\.\w+/.test(externalModule);
 }
---
+++ test/fixtures/with-dot.js
@@ -0,0 +1,1 @@
+const camelCase = require('lodash.camelcase');
